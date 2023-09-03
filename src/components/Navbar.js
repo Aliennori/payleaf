e75bbcb11e83ec774be1d11b91578d2905d40334
Nori Mueni <norikendall@gmail.com>
2023-09-03 22:33:09 +0300

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+
+
+    return ( 
+        <div className="navbar">
+          <a href="/">
+            <img src="../images/logo1.svg" alt="" />
+            <h1>Payleaf</h1>
+            </a>  
+        </div>
+
+     );
+}
+ 
+export default Navbar;
