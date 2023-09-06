ab58ee73288ded5a984bd1e1a0efa7cfbe2044bc
Nori Mueni <norikendall@gmail.com>
2023-09-06 03:19:10 +0300

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,35 +3,35 @@ import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 const Footer = () => {
     return (  
         <div className="bg-[#141414] px-6 md:px-20 py-5 text-white">
-         <div className="md:flex md:justify-between items-center">
+         <div className="md:flex md:justify-between items-center border-b border-[#424242]">
             <div className="text-left">
-                <h1 className="text-2xl sm:text-4xl">Be the first to know when we launch</h1>
-                <p className="text-xl">We're still building. Join our waiting list now! </p>
+                <h1 className="text-2xl mb-4 font-bold">Be the first to know when we launch</h1>
+                <p className="mb-2">We're still building. Join our waiting list now! </p>
 
            </div>
            <div className="">
            <form>
-  <div className="mt-2">
+  <div className="mt-6 md:mt-0 md:flex space-x-2">
     <input
       type="email"
-      className="mr-2 py-3 px-4 border-2 w-2/4"
+      className=" py-3 px-4 border-2 w-full rounded-md md:w-72 md:ml-2"
       placeholder="Enter your email"
     />
     <input
       type="submit"
-      value="Subscribe"
-      className="px-4 bg-tertiary py-3 tracking-wider font-bold text-white"
+      value="Submit"
+      className="px-6 bg-tertiary py-3 tracking-wider font-bold text-white w-full md:w-fit border border-[#A3A3A3]  mt-6 md:mt-0"
     />
   </div>
 </form>
            </div>
          </div>
          <div className="md:flex md:justify-between items-center py-5">
-           <div className="">
+           <div className="text-left">
             <p>2023. All rights reserved</p>            
            </div>
            <div className="">
-            <ul>
+            <ul className="flex space-x-4">
                 <li><a href="/"> <FaInstagram size={24} /></a></li>
                 <li><a href="/"><FaFacebook size={24} /></a></li>
                 <li><a href="/"><FaTwitter size={24} /></a></li>
